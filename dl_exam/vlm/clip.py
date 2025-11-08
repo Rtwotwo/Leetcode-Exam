eb9437e2c31ac6f00b86f39a9746581cfa4cd45e
Rtwotwo <yoloredal@gmail.com>
2025-11-08 23:33:03 +0800

    Update VLA submodules

--- dl_exam/vlm/clip.py
+++ dl_exam/vlm/clip.py
@@ -241,4 +241,46 @@ class VisionTransformer(nn.Module):
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=width, 
                     kernel_size=patch_size, stride=patch_size,bias=False)
         scale = width ** -0.5
+        self.class_embedding = nn.Parameter(scale * torch.randn(width))
+        self.positional_embedding = nn.Parameter(scale * torch.randn((input_resolution // patch_size) ** 2 + 1, width))
+        self.ln_pre = LayerNorm(width)
+        self.transformer = Transformer(width, layers, heads)
+        self.ln_post = LayerNorm(width)
+        self.proj = nn.Parameter(scale * torch.randn(width, output_dim))
+    def forward(self, x:torch.Tensor)->torch.Tensor:
+        x = self.conv1(x) # 形状是[*, width, grid, grid]
+        x = x.reshape(x.shape[0], x.shape[1], -1) # # 形状是[*, width, grid*grid]
+        x = x.permute(0, 2, 1)
+        # 形状为[*, grid ** 2 + 1, width]
+        x = torch.cat([self.class_embedding.to(x.dtype) + 
+                torch.zeros(x.shape[0], 1, x.shape[-1], dtype=x.type, 
+                device=x.device), x], dim=1)
+        x = x + self.positional_embedding.to(x.dtype)
+        x = self.ln_pre(x)
+
+        x = x.permute(1, 0, 2) # NLD -> LND
+        x = self.transformer(x)
+        x = x.permute(1, 0, 2) # LND -> NLD
+        self.ln_post(x[:, 0, :])
+        if self.proj is not None:
+            x = x @ self.proj
+        return x
+
+
+class CLIP(nn.Module):
+    def __init__(self, 
+                 embed_dim: int,
+                #  视觉变量
+                image_resolution: int,
+                vision_layers: Union[Tuple[int, int, int, int], int],
+                vision_width: int, 
+                vision_patch_size: int,
+                # 文本变量
+                context_length: int,
+                vocab_size: int,
+                transformer_width: int,
+                transformer_heads: int,
+                transformer_layers: int
+                )->None:
+        super().__init__()
         
