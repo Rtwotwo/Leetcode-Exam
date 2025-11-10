5eba53f17e0912311284385fed233f4e2b6ba1f9
Rtwotwo <yoloredal@gmail.com>
2025-11-10 16:46:47 +0800

    Add initial parameters of clip

--- dl_exam/vlm/clip.py
+++ dl_exam/vlm/clip.py
@@ -270,7 +270,7 @@ class VisionTransformer(nn.Module):
 class CLIP(nn.Module):
     def __init__(self, 
                  embed_dim: int,
-                #  视觉变量
+                # 视觉变量
                 image_resolution: int,
                 vision_layers: Union[Tuple[int, int, int, int], int],
                 vision_width: int, 
@@ -283,4 +283,61 @@ class CLIP(nn.Module):
                 transformer_layers: int
                 )->None:
         super().__init__()
+        self.context_length = context_length
+        if isinstance(vision_layers, (tuple, list)):
+            vision_heads = vision_width * 32 // 64
+            self.visual = ModifiedResNet(
+                        layers=vision_layers,
+                        output_dim=embed_dim,
+                        heads=vision_heads,
+                        input_resolution=image_resolution,
+                        width=vision_width)
+        else:
+            vision_heads = vision_width // 64
+            self.visual = VisionTransformer(
+                        input_resolution=image_resolution,
+                        patch_size=vision_patch_size,
+                        width=vision_width,
+                        layers=vision_layers,
+                        heads=vision_heads,
+                        output_dim=embed_dim)
+        self.transformer = Transformer(
+            width=transformer_width,
+            layers=transformer_layers,
+            heads=transformer_heads,
+            attn_mask=self.build_attention_mask())
+        self.vocab_size = vocab_size
+        self.toekn_embedding = nn.Embedding(vocab_size, transformer_width)
+        self.positional_embedding = nn.Parameter(torch.empty(self.context_length, transformer_width))
+        self.ln_final = LayerNorm(transformer_width)
         
+        self.text_projection = nn.Parameter(torch.empty(transformer_width, embed_dim))
+        self.logit_scale = nn.Parameter(torch.ones([]) * np.log(1 / 0.07))
+        self.initialize_parameters()
+    def initialize_parameters(self)->None:
+        nn.init.normal_(self.toekn_embedding.weight, std=0.02)
+        nn.init.normal_(self.positional_embedding, std=0.01)
+        if isinstance(self.visual, ModifiedResNet):
+            if self.visual.attnpool is not None:
+                std = self.visual.attnpool.c_proj.in_features ** -0.5
+                nn.init.normal_(self.visual.attnpool.q_proj.weight, std=std)
+                nn.init.normal_(self.visual.attnpool.k_proj.weight, std=std)
+                nn.init.normal_(self.visual.attnpool.v_proj.weight, std=std)
+                nn.init.normal_(self.visual.attnpool.c_proj.weight, std=std)
+            # ResNet模型中特定的BatchNorm层参数进行初始化, 有针对得初始化BN层得权重
+            for resnet_block in [self.visual.layer1, self.visual.layer2, self.visual.layer3, self.visual.layer4]:
+                for name, param in resnet_block.named_parameters():
+                    if name.endswith("bn3.weight"):
+                        nn.init.zeros_(param)
+        
+        proj_std = (self.transformer.width ** -0.5) * ((2 * self.transformer.layers) ** -0.5)
+        attn_std = self.transformer.width ** -0.5
+        fc_std = (2 * self.transformer.width) ** -0.5
+        for block in self.transformer.resblocks:
+            nn.init.normal_(block.attn.in_proj_weight, std=attn_std)
+            nn.init.normal_(block.attn.out_proj.weight, std=proj_std)
+            nn.init.normal_(block.mlp.c_fc.weight, std=fc_std)
+            nn.init.normal_(block.mlp.c_proj.weight, std=proj_std)
+            
+        if self.text_projection is not None:
+            nn.init.normal_(self.text_projection, std=self.transformer.width ** -0.5)
